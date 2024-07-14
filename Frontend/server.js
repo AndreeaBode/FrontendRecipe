be64072a7b9a54cc925687d74a7513bbc3a30f2e
Andreea Bode <92944991+AndreeaBode@users.noreply.github.com>
2024-07-15 00:58:42 +0300

    f

--- Frontend/server.js
+++ Frontend/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/heroku-angular-d'));
+app.use(express.static('/dist/ass'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/heroku-angular-d/'}),
+    res.sendFile('index.html', {root: '/dist/ass'}),
 );
 
 // Start the app by listening on the default Heroku port
